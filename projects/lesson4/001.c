b18604955798792e96c931f0a1a4323621f3f167
emonyr <emonyr@163.com>
2015-03-01 17:40:56 +0800

    added SVN

---
+++ projects/lesson4/001.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*±àÂëº¯Êı*/
+void *encode(void *dest,void *src,unsigned long len)
+{
+	unsigned char *encode_data,*dncode_data;
+	encode_data = (unsigned char)malloc(sizeof())
+}
+
+
+/*½âÂëº¯Êı*/
+void *decode(void *dest,void *src,unsigned long len)
+{
+	
+}
+
+
+
+int main()
+{
+	unsigned char raw_data[9] = {0x11,0x11,0x11,0x22,0x33,0x33,0x33,0x33,0x33};
+
+	
+	
+	return 0;	
+}
