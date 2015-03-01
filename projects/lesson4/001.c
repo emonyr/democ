a15edae182a5f350d42e190251e2b8d6e68efea0
Rui Yang <emonyr@163.com>
2015-03-01 14:13:47 -0500

    Lesson4: finished encode function

--- projects/lesson4/001.c
+++ projects/lesson4/001.c
@@ -1,28 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-/*±àÂëº¯Êı*/
-void *encode(void *dest,void *src,unsigned long len)
+/*ç¼–ç å‡½æ•°*/
+unsigned int encode(unsigned char *dest,unsigned char *src,unsigned long len)
 {
-	unsigned char *encode_data,*dncode_data;
-	encode_data = (unsigned char)malloc(sizeof())
+	if(dest == NULL || src == NULL || len == 0){
+		perror("space error");
+		exit(-1);
+	}
+
+	int i=1;
+	unsigned char *in,*out;
+	in = src;
+	out = dest;
+
+	while(in != (src+len)){
+		*(out++) = *in;
+		while(*in == *(++in)){
+			i++;
+		}
+		*(out++) = i;
+		i = 1;
+	}
+	return out - dest;
 }
 
-
-/*½âÂëº¯Êı*/
+/*è§£ç å‡½æ•°*/
 void *decode(void *dest,void *src,unsigned long len)
 {
 	
 }
 
 
-
 int main()
 {
-	unsigned char raw_data[9] = {0x11,0x11,0x11,0x22,0x33,0x33,0x33,0x33,0x33};
-
+	int i;
+	unsigned int encode_size;
+	unsigned char arr[15] = {0x11,0x11,0x11,0x11,0x22,0x33,0x33,0x33,0x33,0x44,0x44,0x55,0x55,0x55,0x55};
+	unsigned char *encode_dest;
+	encode_dest = (unsigned char *)calloc(1,sizeof(arr));
 	
-	
-	return 0;	
+	encode_size = encode(encode_dest,arr,sizeof(arr));
+	for(i=0;i<encode_size;i++){
+		printf("%x ",encode_dest[i]);
+	}
+	printf("\n");
+	free(encode_dest);
+	return 0;
 }
+
+
+
+
+
+
