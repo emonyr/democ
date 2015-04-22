3eac83501a06d50a45265131c2c5aa4ee62f5500
emonyr <emonyr@163.com>
2015-04-23 03:04:38 +0800

    updated TCP server

--- projects/app/socket/TCP_client.c
+++ projects/app/socket/TCP_client.c
@@ -10,13 +10,13 @@
 #include <signal.h>
 #include <dirent.h>
 #include <fcntl.h>
-#define ERR(x) {perror(x);exit(errno);}
-#define CMDSIZE 512
+#define ERR(x) {send(sockfd,"quit",5,0);perror(x);exit(errno);}
 #define BUFSIZE 128
 
 int sockfd,nbyte;
-char cmd[CMDSIZE];
-char arg[CMDSIZE];
+struct sockaddr_in remote_sock;
+char cmd[BUFSIZE];
+char arg[BUFSIZE];
 char buf[BUFSIZE];
 
 char *getarg(const char *s) //此函数返回s中的第二段非空字符串
@@ -41,22 +41,16 @@ char *getarg(const char *s) //此函数返回s中的第二段非空字符串
 
 int cmd_help()
 {
-    if(recv(sockfd,buf,BUFSIZE,0) == -1){
+    if(recv(sockfd,buf,BUFSIZE,0) == -1)
         ERR("recv");
-    }
     printf("%s\n",buf);
     return 0;
 }
 
 int cmd_list()
 {
-	int total;
-	while(1){
-		recv(sockfd,buf,BUFSIZE,0);
-		if(strcmp(buf,"/EOF") == 0)
-			break;
+	while((nbyte = recv(sockfd,buf,BUFSIZE,0)) > 0)
 		printf("%s\n",buf);
-	}
 
     return 0;
 }
@@ -67,26 +61,22 @@ int cmd_get()
 	getarg(cmd);
 	
 	umask(0);
-	content = open(arg,O_WRONLY|O_CREAT|O_TRUNC,0666);
-	if(content < 0){
+	if((content = open(arg,O_WRONLY|O_CREAT|O_TRUNC,0666)) < 0)
 		ERR("open");
-	}
     
-	while(nbyte = recv(sockfd,buf,BUFSIZE,0)){
+	while(nbyte = recv(sockfd,buf,BUFSIZE,0) > 0){
 		if(strcmp(buf,"file doesn't exist.\n") == 0){
 			printf("file doesn't exist.\n");
-			return -1;
-		}
-		if(strcmp(buf,"/EOF") == 0){
 			close(content);
-			printf("file downloaded\n");
-			return 0;
+			unlink(arg);
+			return -1;
 		}
 		write(content,buf,nbyte);
 	}
 	
 	umask(sval);
 	close(content);
+	printf("file downloaded\n");
 	
     return 0;
 }
@@ -95,18 +85,14 @@ int cmd_put()
 {
 	int content;
     getarg(cmd);
-    printf("%s\n",arg);
 	
     recv(sockfd,buf,BUFSIZE,0);
-    if(strcmp(buf,"failed to create file on server.\n") == 0){
-        printf("failed to create file on server.\n");
+	printf("%s",buf);
+    if(strcmp(buf,"failed to create file on server.\n") == 0)
         return -1;
-    }
     
-	content = open(arg,O_RDONLY);
-	if(content < 0){
+	if((content = open(arg,O_RDONLY)) < 0)
 		ERR("open");
-	}
 	while((nbyte = read(content,buf,BUFSIZE)) > 0){
 		if(send(sockfd,buf,nbyte,0) == -1)
         	perror("send file");
@@ -129,22 +115,18 @@ int dispatch() //根据cmd分派任务
     else if(strncmp(cmd,"put",3) == 0)
         cmd_put();
     else{
-	while(1){
 		recv(sockfd,buf,BUFSIZE,0);
-		if(strcmp(buf,"/EOF") == 0)
-			break;
 		printf("%s\n",buf);
-	}
     }
+	memset(cmd,0,BUFSIZE);
     return 0;
 }
 
 void sig_exit_handler(int signum)   //如果程序异常终止，则通知server
 {
     if(signum == SIGINT || signum == SIGTERM){
-        if(send(sockfd,"quit",5,0) == -1){
+        if(send(sockfd,"quit",4,0) == -1)
             ERR("send quit");
-        }
     }
     exit(1);
 }
@@ -156,28 +138,19 @@ int main(int argc,char **argv)
 		printf("Usage: %s + <hostname>\n",argv[0]);exit(1);
 	}
 
-	
-	struct sockaddr_in remote_sock;
 	struct hostent *myhost;
-	
-	myhost = gethostbyname(argv[1]);
-	if(myhost == NULL){
+	if((myhost = gethostbyname(argv[1])) == NULL)
 		ERR("gethostbyname");
-	}
-	printf("%s\n",myhost->h_addr);
 	
 	remote_sock.sin_family = AF_INET;
 	remote_sock.sin_port = htons(8080);
 	inet_aton(myhost->h_addr,(struct in_addr *)&remote_sock.sin_addr);
 	
-	sockfd = socket(AF_INET,SOCK_STREAM,0);
-	if(sockfd == -1){
+	if((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1)
 		ERR("sockfd");
-	}
-	if(connect(sockfd,(struct sockaddr *)&remote_sock,sizeof(remote_sock)) == -1){
+	if(connect(sockfd,(struct sockaddr *)&remote_sock,sizeof(remote_sock)) == -1)
 		ERR("connect");
-	}
-    
+	
     if(signal(SIGINT,sig_exit_handler) == SIG_ERR)
         ERR("signal SIGINT");
     if(signal(SIGTERM,sig_exit_handler) == SIG_ERR)
@@ -186,10 +159,8 @@ int main(int argc,char **argv)
 	while(strncmp(cmd,"quit",4) != 0){
         dispatch();
         printf("\n>");
-		fgets(cmd,CMDSIZE,stdin);
-		if(send(sockfd,cmd,CMDSIZE,0) == -1){
-			ERR("send cmd");
-		}
+		fgets(cmd,BUFSIZE,stdin);
+		send(sockfd,cmd,BUFSIZE,0);
 	}
 	close(sockfd);
 	
--- projects/app/socket/TCP_server.c
+++ projects/app/socket/TCP_server.c
@@ -18,14 +18,13 @@
 #include <fcntl.h>
 #define ERR(x) {perror(x);exit(errno);}
 #define BUFSIZE 128
-#define CMDSIZE 512
 #define MAXCONNECT 10
 
 int client_fd,*client_count,nbyte;
 socklen_t client_len;
-char cmd[CMDSIZE];
-char arg[CMDSIZE];
-char buf[BUFSIZE];
+char cmd[BUFSIZE];
+char arg[BUFSIZE];
+char buf[BUFSIZE] = "\nWelcome to Johnny's server!\n\nPlease input a command.\n";
 
 
 char *getarg(const char *s) //此函数返回s中的第二段非空字符串
@@ -50,9 +49,8 @@ char *getarg(const char *s) //此函数返回s中的第二段非空字符串
 
 int cmd_help()
 {
-    if(send(client_fd,"Usage:\n\thelp\n\tlist\n\tget <file>\n\tput <file>\n\tquit\n",CMDSIZE,0) == -1){
+    if(send(client_fd,"Usage:\n\thelp\n\tlist\n\tget <file>\n\tput <file>\n\tquit\n",BUFSIZE,0) == -1)
         perror("send help");
-    }
     return 0;
 }
 
@@ -63,18 +61,18 @@ int filter(const struct dirent *d)
 
 int cmd_list()
 {
-    int count,i;
+    int count,i=0;
     struct dirent **namelist;
     
-    count = scandir(".",&namelist,filter,alphasort);
-    if(count == -1){
+    if((count = scandir(".",&namelist,filter,alphasort)) == -1)
         perror("scandir");
-    }
     for(i=0;i<count;i++){
-        if(send(client_fd,namelist[i]->d_name,BUFSIZE,0) == -1)
-		ERR("send");
-    } 
-	send(client_fd,"/EOF",5,0);
+		if ((strcmp(namelist[i]->d_name, ".") == 0) || (strcmp(namelist[i]->d_name, "..") == 0) || (namelist[i]->d_name == NULL))
+			continue;
+		strcpy(buf,namelist[i]->d_name);
+        if(send(client_fd,buf,BUFSIZE,0) == -1)
+			ERR("send");
+    }
 
     return 0;
 }
@@ -91,14 +89,13 @@ int cmd_get()
         close(content);
         exit(1);
 	}
-	while((nbyte = read(content,buf,BUFSIZE)) > 0){
+	while((nbyte = read(content,buf,BUFSIZE)) != 0){
         	if(send(client_fd,buf,nbyte,0) == -1)
-            perror("send file");
+				perror("send file");
 	}
-	send(client_fd,"/EOF",5,0);
-
-    umask(sval);
+	
     close(content);
+	umask(sval);
     
     return 0;
 }
@@ -112,15 +109,18 @@ int cmd_put()
     content = open(arg,O_WRONLY|O_CREAT|O_TRUNC,0666);
     if(content < 0){
         send(client_fd,"failed to create file on server.\n",BUFSIZE,0);
-	unlink(arg);
+		unlink(arg);
         exit(1);
     }
+	else
+		send(client_fd,"Transferring file.\n",BUFSIZE,0);
     
-	while((nbyte = recv(client_fd,buf,BUFSIZE,0)) > 0){
+	while((nbyte = recv(client_fd,buf,BUFSIZE,0)) != 0){
 		write(content,buf,nbyte);
 	}
-    umask(sval);
+    
     close(content);
+	umask(sval);
 	
     return 0;
 }
@@ -137,70 +137,69 @@ int dispatch() //根据cmd分派任务
         cmd_get();
     else if(strncmp(cmd,"put",3) == 0)
         cmd_put();
-    else if(send(client_fd,"Unknown command\nInput \"help\" for more information\n",CMDSIZE,0) == -1){
+	else if(strncmp(cmd,"quit",4) == 0)
+        return 0;
+    else if(send(client_fd,"Unknown command\nInput \"help\" for more information\n",BUFSIZE,0) == -1)
         ERR("send Unknown command");
-    }
+	memset(cmd,0,sizeof(cmd));
     return 0;
 }
 
 void *set_client_count(const char *filename)    //实现连接计数器
 {
-    int shm_id;
+    int shm_id,sval;
     void * ret;
     
-    umask(0);
+    sval = umask(0);
     //从内核空间分配shm
-    shm_id = shmget(ftok(filename,'a'),sizeof(int),IPC_CREAT|0666);
-    if (shm_id == -1) {
-        perror("shmget");exit(errno);
-    }
+    if ((shm_id = shmget(ftok(filename,'a'),sizeof(int),IPC_CREAT|0666)) == -1)
+        ERR("shmget");
     //绑定shm到用户空间
-    ret = shmat(shm_id,NULL,0);
-    if (ret == (void *)-1) {
-        perror("shmat");exit(errno);
-    }
-    umask(0002);
+    if ((ret = shmat(shm_id,NULL,0)) == (void *)-1)
+        ERR("shmat");
+    umask(sval);
     
     return ret;
 }
 
+void print_time()
+{
+	time_t timestamp;
+	struct tm *showtime;
+
+	time(&timestamp);
+    showtime = localtime(&timestamp);
+    printf("%02d:%02d:%02d -- %d client connected\n",showtime->tm_hour,showtime->tm_min,showtime->tm_sec,*client_count);
+}
 
 
 int main(int argc,const char **argv)
 {
 	int server_fd,reuse=0,cpid;
-    time_t timestamp;
-    struct tm *showtime;
 
 	struct sockaddr_in server_sock;
 	struct sockaddr_in client_sock;
 	
+	memset(&server_sock,0,sizeof(server_sock));
 	server_sock.sin_family = AF_INET;
 	server_sock.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_sock.sin_port = htons(8080);
 	
     //获取socket
-    server_fd = socket(AF_INET,SOCK_STREAM,0);
-	if(server_fd == -1){
+	if((server_fd = socket(AF_INET,SOCK_STREAM,0)) == -1)
 		ERR("server_fd");
-	}
     //绑定server_fd
-    if(bind(server_fd,(struct sockaddr *)&server_sock,sizeof(server_sock)) == -1){
+    if(bind(server_fd,(struct sockaddr *)&server_sock,sizeof(server_sock)) == -1)
         ERR("bind");
-    }
     //设置server_fd端口可以被重复使用
-    if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse)) == -1){
+    if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse)) == -1)
         ERR("setsockopt");
-    }
     //监听server_fd
-	if(listen(server_fd,MAXCONNECT) == -1){
+	if(listen(server_fd,MAXCONNECT) == -1)
 		ERR("listen");
-	}
     //设置连接计数器
-    client_count = (int *)set_client_count(argv[0]);
-    if(!client_count){
+    if((client_count = (int *)set_client_count(argv[0])) == NULL)
         ERR("set_client_count");
-    }
     *client_count = 0;
     
 
@@ -212,33 +211,25 @@ Listen:
     }while(client_fd == -1);
 
     (*client_count)++;	//更新连接计数器
-    time(&timestamp);
-    showtime = localtime(&timestamp);
-    printf("%02d:%02d:%02d -- %d client connected\n",showtime->tm_hour,showtime->tm_min,showtime->tm_sec,*client_count);
+	print_time();
     
     cpid = fork();  //开辟子进程与客户端通信
     if(cpid == -1){
         ERR("fork");
-    }
+	}
     else if(cpid == 0){
-        if(send(client_fd,"Welcome to Johnny's server!\n\nPlease input a command.\n",sizeof("Welcome to Johnny's server!\n\nPlease input a command.\n"),0) == -1){
-            ERR("send welcome");
-        }
-	send(client_fd,"/EOF",5,0);
+        send(client_fd,buf,BUFSIZE,0);
 
-	//等待client输入命令
-        while(strncmp(cmd,"quit",4) != 0){
-            while(recv(client_fd,cmd,CMDSIZE,0) == -1);
-            dispatch();
-        }
-        close(client_fd);
-
-        (*client_count)--;	//更新连接计数器
-        printf("Client quit.\n");
-        time(&timestamp);
-        showtime = localtime(&timestamp);
-        printf("%02d:%02d:%02d -- %d client connected\n",showtime->tm_hour,showtime->tm_min,showtime->tm_sec,*client_count);
-        return 0;
+		//等待client输入命令
+		while(strncmp(cmd,"quit",4) != 0){
+			while(recv(client_fd,cmd,BUFSIZE,0) == -1);
+			printf("%s\n",cmd);
+			dispatch();
+		}
+		close(client_fd);
+		(*client_count)--;	//更新连接计数器
+		print_time();
+		return 0;
     }
     else{
         close(client_fd);
