5edffaa3fd1f2ef95b5e80f61b8551dc3382cfd3
emonyr <emonyr@163.com>
2015-04-27 20:10:34 +0800

    added Makefile for list project

--- projects/list/include/list.h
+++ projects/list/include/list.h
@@ -10,4 +10,5 @@ extern struct list_item *list_new_item(int data);
 extern int list_show(struct list_item *origin);
 extern int list_insert(struct list_item *new_item,struct list_item *insert_point,int offset);
 extern int list_delete(struct list_item *delete_point,int offset);
+extern int list_len(struct list_item *head);
 extern int list_reverse(struct list_item *head);
