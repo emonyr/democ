af936ff176ab949fd30f8e2b230a1d2aa437e949
emonyr <emonyr@163.com>
2015-04-29 13:25:40 +0800

    fix bug of joseph_circle

--- projects/list/joseph_circle.c
+++ projects/list/joseph_circle.c
@@ -5,7 +5,7 @@
 int main()
 {
 	int i,n=8,k=3,m=4;
-	struct list_item *head,*tail;
+	struct list_item *head,*tmp;
 	head = list_new_item(1);
 
 	//create the circle
@@ -13,27 +13,31 @@ int main()
 		list_insert(list_new_item(i),head,0);
 	list_reverse(head);
 	list_show(head);
-	tail = head;
-	while(tail->next != NULL)
-		tail = tail->next;
-	tail->next = head;	//首尾相连形成环
+	tmp = head;
+	while(tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = head;	//首尾相连形成环
 
-	printf("mark\n\n");
 	//把head移动到第k项
 	for(i=1;i<k;i++)
 		head = head->next;
 	//数到m的人出列
 	printf("out list: ");
-	while(head->next != NULL){
+	while(head != NULL){
 		for(i=1;i<m-1;i++)
 			head = head->next;
-		if(head->next != NULL)
-			printf("%d  ",head->next->data);
-		tail = head->next;
-		head->next = head->next->next;
-		if(tail != NULL)
-			free(tail);
-		head = head->next;
+        if(head != head->next->next){
+			printf("%d ",head->next->data);
+        }
+        else{
+            printf("%d ",head->next->data);
+            printf("%d ",head->data);
+            break;
+        }
+		tmp = head->next;
+        head->next = head->next->next;
+        free(tmp);
+        head = head->next;
 	}
 	printf("\n");
 
