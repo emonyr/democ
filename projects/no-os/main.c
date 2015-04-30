72bca968b6527d78ef45822e65d1666da559e714
emonyr <emonyr@163.com>
2015-04-30 22:15:07 +0800

    changed project structure

--- projects/no-os/main.c
+++
@@ -1,9 +0,0 @@
-#define GPJ2CON_0 (*(volatile unsigned long *)0xE0200280)
-#define GPJ2DAT_0 (*(volatile unsigned long *)0xE0200284)
-
-int main()
-{
-	GPJ2CON_0 = 0x00001111;
-	GPJ2DAT_0 = 0;
-	return 0;
-}
