94fdb7825b76dd4ca0a51177d4ca2b4532a949c4
emonyr <emonyr@163.com>
2015-04-30 14:33:23 +0800

    updated joseph_circle.c

--- projects/list/joseph_circle.c
+++ projects/list/joseph_circle.c
@@ -5,18 +5,14 @@
 int main()
 {
 	int i,n=8,k=3,m=4;
-	struct list_item *head,*tmp;
-	head = list_new_item(1);
+	struct list_item *head;
 
 	//create the circle
+	head = list_new_item(1);
+	head->next = head;
 	for(i=2;i<=n;i++)
-		list_insert(list_new_item(i),head,0);
-	list_reverse(head);
-	list_show(head);
-	tmp = head;
-	while(tmp->next != NULL)
-		tmp = tmp->next;
-	tmp->next = head;	//首尾相连形成环
+		list_insert(list_new_item(i),head,i-2);
+
 
 	//把head移动到第k项
 	for(i=1;i<k;i++)
@@ -31,12 +27,12 @@ int main()
         }
         else{
             printf("%d ",head->next->data);
-            printf("%d ",head->data);
+			free(head->next);
+			printf("%d ",head->data);
+			free(head);
             break;
         }
-		tmp = head->next;
-        head->next = head->next->next;
-        free(tmp);
+		list_delete(head,0);
         head = head->next;
 	}
 	printf("\n");
