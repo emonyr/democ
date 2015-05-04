6f95e8a4c2da7033a8520676bfef3b95e0b09f4a
emonyr <emonyr@163.com>
2015-05-04 14:42:27 +0800

    added map.lds

--- arm/main.c
+++ arm/main.c
@@ -1,9 +1,20 @@
 #define GPJ2CON_0 (*(volatile unsigned long *)0xE0200280)
 #define GPJ2DAT_0 (*(volatile unsigned long *)0xE0200284)
 
+void mydelay(int input)
+{
+	volatile int count = input;
+	while(count--);
+}
+
 int main()
 {
-	GPJ2CON_0 = 0x00001111;
-	GPJ2DAT_0 = 0;
+		GPJ2CON_0 = 0x00000011;
+		
+		while(1){
+			GPJ2DAT_0 = 0;
+			mydelay(100000);
+			GPJ2DAT_0 = 0xf;
+		}
 	return 0;
 }
