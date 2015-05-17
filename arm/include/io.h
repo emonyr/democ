ec9030277c4e50ad61bba7d113dab73108aea46f
emonyr <emonyr@163.com>
2015-05-17 20:12:00 +0800

    updated io.h

--- arm/include/io.h
+++ arm/include/io.h
@@ -1,10 +1,15 @@
 #ifndef IO_H
 #define IO_H
 
-#define readl(a) (*(volatile unsigned long *)(a))
-#define writel(v,a) ((*(volatile unsigned long *)(a)) = v)
-#define __REG(a) (*(volatile unsigned long *)(a))
+#define __REG(a) (*(volatile unsigned int *)(a))
 
+#define readl(a) (*(volatile unsigned long *)(a))
+#define readb(a) (*(volatile unsigned char *)(a))
+#define readw(a) (*(volatile unsigned short *)(a))
+
+#define writel(v,a) ((*(volatile unsigned long *)(a)) = v)
+#define writeb(v,a) ((*(volatile unsigned char *)(a)) = v)
+#define writew(v,a) ((*(volatile unsigned short *)(a)) = v)
 
 
 
